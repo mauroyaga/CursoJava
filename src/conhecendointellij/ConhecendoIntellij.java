7303d5e17adb1d167560c0b65a516f9a43cf135e
Mauro Yaga <mauro.yaga@outlook.com>
2023-02-26 12:17:39 -0300

    CriacaoConhecendoIntellij

---
+++ src/conhecendointellij/ConhecendoIntellij.java
@@ -0,0 +1,36 @@
+/*
+Nome do Programa: Reconhecimento da IDE
+Descrição do Programa: Esta é minha primeira interação com a IDE Intellij e aprimeira impressão é que se trata de
+uma IDE de alta técnologia, com diversas inteligencias de suporte. Um exemnplo é que com 20 min fazendo comentários
+em português ela me mostrou um pop-up oferecendo dowload da extenção da lingua portugesa. E junto um corretor gramatical
+da lingua portuguesa.
+
+COMANDOS INICIAIS
+ctrl shift A: para buscador de configurações do sitemas;
+
+
+RODAR A APLICAÇÃO
+Para rodar uma aplicação java é necessário configurar o ambiente, mas pode rodar com as configurações
+padões.
+
+
+Nome do Autor: Mauro Cesar Yaga Junior
+Data:
+ */
+
+package conhecendointellij; //diretório virtual que contém as classes do programa. A coesão do sistema é mantido
+//com a separação das classes que tenham forte conexões lógicas entre si.
+
+public class ConhecendoIntellij {   //, a classe "AulaUm" é definida com o modificador "public", o que significa que ela pode ser
+    // acessada a partir de qualquer outra classe.o que significa que ela pode ser acessada a partir de qualquer outra classe.
+    public static void main(String[] args) { //Dentro da classe, há um método chamado "main",
+        //que é o ponto de entrada do programa. O método "main" é o primeiro método a ser executado quando o programa é iniciado.
+
+        System.out.println("Hello World"); //O método "main" recebe um parâmetro do tipo "String[]" chamado "args", que é uma matriz de
+        //strings que pode ser usada para passar argumentos para o programa a partir da linha de comando.
+
+        System.out.println("Esta saíde de texto é uma string passada como paramentro da função: System.out.printl()");
+
+    }
+
+}
